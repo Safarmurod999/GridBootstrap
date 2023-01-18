d80f37ad97c55662aa8d89cd7e0362d3064b9b38
Safarmurod999 <supermen0904@gmail.com>
2023-01-18 16:26:52 +0500

    main

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.navbar-toggler');
+const navTop = document.getElementById('top-nav');
+
+function navToggle(){
+    navTop.classList.toggle('toggle-nav');
+}
+console.log(btn);
+
+console.log(navTop);
